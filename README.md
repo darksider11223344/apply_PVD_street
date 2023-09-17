e0b01afa3f075e5a2ad2cb8ad5319d354ef58e07
“Binh <darksider11223344@gmail.com>
2023-09-17 17:11:02 +0700

    adding vehicle dynamically

--- README.md
+++ README.md
@@ -4,8 +4,27 @@ Create Table tblVehicles
 (
 ID int NOT NULL IDENTITY(1,1),
 VerhicleID nvarchar(50) NOT NULL,
-TimeStampUTC DATETIME NULL,
+TimeStampUTC DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 RouteId nvarchar(50) NOT NULL,
 DepartLane nvarchar(50) NOT NULL,
-TypeID nvarchar(50) NOT NULL
+TypeID nvarchar(50) NOT NULL,
+Active bit NOT NULL
 )
+
+--- insert for demo
+USE [PVD_traffic_data]
+GO
+
+INSERT INTO [dbo].[tblVehicles]
+           ([VerhicleID]
+           ,[RouteId]
+           ,[DepartLane]
+           ,[TypeID]
+           ,[Active])
+     VALUES
+           ('vehicle_1'
+           ,'route_0'
+           ,'free'
+           ,'vtype_car1'
+           ,0)
+GO
--- demo.py
+++ demo.py
@@ -1,14 +1,15 @@
 import traci
 import time
 import pyodbc
+import datetime as dt
 
 import functions as fn
 
 sumoCmd = ["sumo-gui", "-c", "demo.sumocfg"]
 
-
 # Change the name of your SERVER in SERVER=KKUSIC-L\SQLEXPRESS (see your database), and UID=FPZ\kkusic (used by SQL authentication)
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-Q9818VV\SQLEXPRESS;DATABASE=PVD_traffic_data;UID=DESKTOP-Q9818VV\quang;TRUSTED_CONNECTION=yes')
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-Q9818VV\SQLEXPRESS;DATABASE=PVD_traffic_data;UID=DESKTOP-Q9818VV\quang;TRUSTED_CONNECTION=yes')
 time.sleep(0.1)
 
 hours = 24
@@ -24,25 +25,50 @@ vehicle_id = "vehicle_1"  # Unique ID for the vehicle
 vehicle_type = "vtype_car1"  # Type of vehicle (you need to define this type in your SUMO configuration)
 route_id = "route_0"  # ID of the route the vehicle will follow
 
-
 # Add a vehicle to the simulation
 # traci.vehicle.add(vehicle_id, route_id, departLane="free", typeID=vehicle_type)
 
 # while (traci.simulation.getMinExpectedNumber() > 0):
 while True:
-    if step % 100 == 0:
-        traci.vehicle.add(vehicle_id + str(step), route_id, departLane="free", typeID=vehicle_type)
+    if step % 50 == 0:
+        # Get a list of all currently existing vehicle IDs
+        existing_vehicle_ids = traci.vehicle.getIDList()
+
+        current_utc = dt.datetime.utcnow()
+        current_time = (current_utc - dt.timedelta(minutes=1)).strftime('%Y-%m-%d %H:%M')
+        select_vehicle_E = """SELECT TOP 1 ID, VerhicleID, RouteId, DepartLane, TypeID FROM tblVehicles WHERE Active = 0 ORDER BY ID ASC"""
+        cursor = conn.cursor()
+        cursor.execute(select_vehicle_E)
+        for row in cursor:
+            # X_0224_01[0] = row[0]
+            # vehicle_id = row[1] + str(step) # perform test
+            vehicle_id = row[1]
+            route_id = row[2]
+            depart_lane = row[3]
+            vehicle_type = row[4]
+
+        time.sleep(0.1)
+
+        if vehicle_id in existing_vehicle_ids:
+            continue
+
+        print(vehicle_id, route_id, depart_lane, vehicle_type)
+        traci.vehicle.add(vehicle_id, route_id, departLane=depart_lane, typeID=vehicle_type)
+
+        update_vehicle_E = """UPDATE tblVehicles SET Active = 1 WHERE VerhicleID = '""" + vehicle_id + """' """
+        print('--- Update >>> ', update_vehicle_E)
+        cursor.execute(update_vehicle_E)
+        conn.commit()
+
     traci.simulationStep()
-    # print(step)
+    print(step)
 
     oldVehIDs_E_in = []
-
 
     if step % (res_time * (1 / step_length)) == 0:
         flow_E_in, speed_E_in, oldVehIDs_E_in, newVehIDs_E_in = \
             fn.edgeVehParameters('404516659#1', '404516659#2', oldVehIDs_E_in)
         # print(flow_E_in, speed_E_in, oldVehIDs_E_in, newVehIDs_E_in)
-
 
     # setFlow(self, calibratorID, begin, end, vehsPerHour, speed, typeID, routeID, departLane='first', departSpeed='max')
     traci.calibrator.setFlow("caliCar1_E_W_0", 0, 60, 180, 1, \
